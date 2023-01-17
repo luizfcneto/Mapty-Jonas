c64a9992cad3b3683e395323a895079e5f3f2171
luizfcneto <luizfcneto123@gmail.com>
2023-01-16 23:56:24 -0300

    +starting navigator.geolocation api

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+const getCurrentPositionCallbackSuccess = (position) => {
+    console.log(position);
+};
+
+const getCurrentPositionCallbackFailure = (position) => {
+    alert("Couldn't get your position");
+};
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        getCurrentPositionCallbackSuccess,
+        getCurrentPositionCallbackFailure
+    );
+}
