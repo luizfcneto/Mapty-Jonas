892aae377489cf2fb825ff4098c51e8cfb08c1f7
luizfcneto <luizfcneto123@gmail.com>
2023-01-20 00:12:27 -0300

    *rendering input form

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+let map, mapEvent;
+
 const getCurrentPositionCallbackSuccess = (position) => {
     console.log(position);
     const { latitude } = position.coords;
@@ -19,29 +21,18 @@ const getCurrentPositionCallbackSuccess = (position) => {
     console.log(mapsTemplate);
 
     const coords = [latitude, longitude];
-    const map = L.map("map").setView(coords, 13);
+    map = L.map("map").setView(coords, 13);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         maxZoom: 19,
         attribution:
             '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
 
-    map.on("click", (event) => {
-        const { lat } = event.latlng;
-        const { lng } = event.latlng;
-        console.log(lat, lng);
-        let marker = L.marker([lat, lng])
-            .addTo(map)
-            .bindPopup(
-                L.popup({
-                    maxWidth: 100,
-                    minWidth: 50,
-                    autoClose: false,
-                    className: "running-popup",
-                })
-            )
-            .setPopupContent("<p> Opa World! </p>")
-            .openPopup();
+    // Handling clicks on map
+    map.on("click", (mapE) => {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
     });
 };
 
@@ -55,3 +46,35 @@ if (navigator.geolocation) {
         getCurrentPositionCallbackFailure
     );
 }
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Clear input fields
+    inputDistance.value = "";
+    inputCadence.value = "";
+    inputDuration.value = "";
+    inputElevation.value = "";
+
+    const { lat } = mapEvent.latlng;
+    const { lng } = mapEvent.latlng;
+    console.log(lat, lng);
+    let marker = L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+            L.popup({
+                maxWidth: 100,
+                minWidth: 50,
+                autoClose: false,
+                className: "running-popup",
+            })
+        )
+        .setPopupContent("<p> Opa World! </p>")
+        .openPopup();
+});
+
+inputType.addEventListener("change", function () {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+    // inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+});
